4e0f66c35c626e7508a76bc51f070265e0517ab8
nruchs <naaty1xd@gmail.com>
2025-07-29 03:19:45 -0300

    Create Alpaca Project

--- src/main/java/com/alpacagod/AlpacaConfig.java
+++ src/main/java/com/alpacagod/AlpacaConfig.java
@@ -5,16 +5,14 @@ import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("alpacagod")
-public interface AlpacaConfig extends Config
-{
-	@ConfigItem(
-		keyName = "petCount",
-		name = "Pet Count",
-		description = "How many times you have petted the alpaca.",
-		hidden = true
-	)
-	default int petCount()
-	{
-		return 0;
-	}
+public interface AlpacaConfig extends Config {
+    @ConfigItem(
+            keyName = "petCount",
+            name = "Pet Count",
+            description = "How many times you have petted the alpaca.",
+            hidden = true
+    )
+    default int petCount() {
+        return 0;
+    }
 }
--- src/test/java/com/alpacagod/PluginLauncher.java
+++ src/test/java/com/alpacagod/PluginLauncher.java
@@ -3,11 +3,9 @@ package com.alpacagod;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class PluginLauncher
-{
-	public static void main(String[] args) throws Exception
-	{
-		ExternalPluginManager.loadBuiltin(AlpacaPlugin.class);
-		RuneLite.main(args);
-	}
+public class PluginLauncher {
+    public static void main(String[] args) throws Exception {
+        ExternalPluginManager.loadBuiltin(AlpacaPlugin.class);
+        RuneLite.main(args);
+    }
 }
